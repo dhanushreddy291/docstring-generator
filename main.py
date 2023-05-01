79b5ad08df778e3a4cb852fefa89205d2071f1b5
GitHub Action <action@github.com>
2023-05-01 13:40:49 +0000

    Add docstrings to .py files

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def add(a, b):
+    """
+    This function takes two arguments, 'a' and 'b', and returns their sum.
+    """
     return a + b
 
 
 def subtract(a, b):
+    """
+    This function takes two arguments, 'a' and 'b', and returns the difference between 'a' and 'b'.
+    """
     return a - b
 
 
 def multiply(a, b):
+    """
+    This function takes two arguments, 'a' and 'b', and returns their product.
+    """
     return a * b
