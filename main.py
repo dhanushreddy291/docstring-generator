e89457e5177eba82534c9e391b81c1033dc3ac1e
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-05-01 19:17:58 +0530

    Checking caching

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ def multiply(a, b):
     """
     return a * b
 
+def print(a):
+    print(a)
 
 def divide(a, b):
     """
     This function takes two arguments, a and b, and returns the result of dividing a by b. If b is equal to zero, it returns the string 'Divide by Zero, Infinity' to avoid a ZeroDivisionError.
     """
     return a / b if b != 0 else "Divide by Zero, Infinity"
+
