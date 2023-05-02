0e227df590cba4f05ba88e9432038ef8f63aa912
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-05-02 16:48:01 +0530

    Checking pip cache

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-def sqrt(a):
-    """
-    Calculate the square root of a given number.
+def pow(a, b):
+    return a ** b
 
-    Args:
-        a (int or float): The number to calculate the square root of.
-
-    Returns:
-        float: The square root of the given number.
-
-    """
-    return a**0.5
+def mul(a, b):
+    return a * b
