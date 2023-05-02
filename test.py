a112db0751f1d88f32a1ce65726ee3999e6d2df7
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-05-02 11:33:28 +0530

    New Code

--- test.py
+++ test.py
@@ -1,43 +1,54 @@
-import time
-
-import redis
-from flask import Flask
-
+from flask import Flask, jsonify, current_app
+from flask_cors import CORS
+from redis import Redis
+from random import randint
+import os
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+_redis = Redis(host=os.getenv('REDIS_HOSTNAME'), port=6379, password=os.getenv("REDIS_PASSWORD")) 
+_redis_pubsub = _redis.pubsub()
+_redis_pubsub.subscribe('my-first-channel')
 
+@app.before_request
+def checkConnectionRedis():
+    global _redis, _redis_pubsub
+    current_app.logger.info("---before_request---")
+    try:
+        _redis.ping()
+    except _redis.ConnectionError:
+        redis = Redis(host='redis', port=6379)
+        _redis_pubsub = _redis.pubsub()
+        _redis_pubsub.subscribe('my-first-channel')
 
-def get_hit_count():
-    """
-    
-    Increments the hit count in the cache and returns the updated count. If there is a connection error with Redis, retries up to 5 times before raising the exception. 
-    
-    Returns:
-        int: The updated hit count in the cache.
-    
-    """
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+def getcounter():
+    return _redis.get('web_counter') and int(_redis.get('web_counter').decode("utf-8")) or 0
 
+@app.route('/counter', methods=["GET"])
+def get():
+    return jsonify(web_counter=getcounter())
 
-@app.route('/')
-def hello():
-    """
-    A Flask route that returns a greeting message along with the hit count retrieved from the `get_hit_count()` function.
-    
-    Returns:
-        str: A greeting message with the hit count.
-    """
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+@app.route('/counter', methods=["POST"])
+def create():
+    _redis.incr('web_counter')
+    return ""
+
+@app.route('/counter', methods=["DELETE"])
+def remove():
+    _redis.set('web_counter', 0)
+    return ""
+
+@app.route('/message', methods=["GET"])
+def getMessage():
+    current_app.logger.info("recieve data: " + str(_redis_pubsub.get_message()))
+    return jsonify(message=str(_redis_pubsub.get_message()))
+
+@app.route('/message', methods=["POST"])
+def createMessage():
+    data = 'data ' + str(randint(0,10))
+    current_app.logger.info("publish data: " + str(data))
+    _redis.publish('my-first-channel', data)
+    return ""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
