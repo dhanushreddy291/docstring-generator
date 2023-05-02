9e5035443a4a1def0d506dac514520ff647d93cf
GitHub Action <action@github.com>
2023-05-02 11:14:05 +0000

    Add docstrings to .py files

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 def mul(a, b):
+    """
+    Returns the product of two numbers.
+
+    Args:
+        a (int or float): First number.
+        b (int or float): Second number.
+
+    Returns:
+        The product of a and b.
+
+    """
     return a * b
