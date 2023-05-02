188f66900aadfc41501179357b8510fe3dffd2e3
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-05-02 16:46:07 +0530

    Update cache

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-def mul(a, b):
-    """
-    Returns the product of two numbers.
-
-    Args:
-        a (int or float): First number.
-        b (int or float): Second number.
-
-    Returns:
-        The product of a and b.
-
-    """
-    return a * b
+def sqrt(a):
+    return a ** 0.5
