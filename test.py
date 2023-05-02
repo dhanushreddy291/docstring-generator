e03341688439e27a03d270603de9e672073ea502
GitHub Action <action@github.com>
2023-05-02 11:16:38 +0000

    Add docstrings to .py files

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 def sqrt(a):
-    return a ** 0.5
+    """
+    Calculate the square root of a given number.
+
+    Args:
+        a (int or float): The number to calculate the square root of.
+
+    Returns:
+        float: The square root of the given number.
+
+    """
+    return a**0.5
