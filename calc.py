58d8a50f40ff5890930cfbd8d7ebbdf6bd2cc8de
GitHub Action <action@github.com>
2023-05-02 06:18:36 +0000

    Add docstrings to .py files

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 
 # creating basic root window
 root = Tk()
-root.title('MyMath')
+root.title("MyMath")
 root.geometry("500x500")
 root.configure(background="light blue")
 
@@ -13,6 +13,9 @@ root.configure(background="light blue")
 
 
 def area():
+    """
+    This function calculates the area of a selected shape based on user input. The user can select from Rectangle, Square, Triangle, and Circle. The function takes the input values for the shape and calculates the area using the myArea function. The calculated area is then displayed using a message box. If an invalid shape is selected, the function displays a warning message.
+    """
 
     SelectedShape = shapes.get()
 
@@ -40,10 +43,14 @@ def area():
     else:
         messagebox.showwarning("Invalid Input", "shape is not available")
 
+
 # defining volume function
 
 
 def volume():
+    """
+    This function calculates the volume of a selected solid shape based on user input. The user can select from Sphere, Cone, Cube, Cylinder, and Cuboid. The function takes the input values for the solid shape and calculates the volume using the myvolume function. The calculated volume is then displayed using a message box. If an invalid solid shape is selected, the function displays a warning message.
+    """
     selected_solid_shape = solidshape.get()
     if selected_solid_shape == "Sphere":
         r = int(radius_entry2.get())
@@ -78,7 +85,11 @@ def volume():
 
 # defining condition_check function
 
+
 def condition_check():
+    """
+    This function checks for a given condition based on user input. The user can select from Pythagorean Triplet Checker and Complimentary&Supplementary Angles. If Pythagorean Triplet Checker is selected, the function takes input values for three sides of a triangle and checks if they form a Pythagorean triplet. If Complimentary&Supplementary Angles is selected, the function takes an input value for an angle and calculates its complimentary and supplementary angles using the mycondition function. The result is not displayed in either case.
+    """
     tocheck = conditions.get()
     if tocheck == "Pythagorean Triplet Checker":
         a = int(side1_entry.get())
@@ -92,23 +103,27 @@ def condition_check():
 
     elif tocheck == "Complimentary&Supplementary Angles":
         angle = int(angle_entry.get())
-        comsup = mycondition(tocheck, 0, 0, 0, angle)
+        mycondition(tocheck, 0, 0, 0, angle)
 
 
 # creating tabs
+
 note1 = ttk.Notebook(root)
 note1.pack(pady=5)
 
 # creating 3 frames
-area_frame = Frame(note1, width=300, height=300,bg="Dark blue")
-volume_frame = Frame(note1, width=300, height=300,bg="Dark blue")
-condition_frame = Frame(note1, width=300, height=300,bg="Dark blue")
+area_frame = Frame(note1, width=300, height=300, bg="Dark blue")
+volume_frame = Frame(note1, width=300, height=300, bg="Dark blue")
+condition_frame = Frame(note1, width=300, height=300, bg="Dark blue")
 area_frame.pack(fill="both", expand=1)
 volume_frame.pack(fill="both", expand=1)
 condition_frame.pack(fill="both", expand=1)
 
 # adding tabs
-note1.add(area_frame, text="Area Calculator",)
+note1.add(
+    area_frame,
+    text="Area Calculator",
+)
 note1.add(volume_frame, text="Volume Calculator")
 note1.add(condition_frame, text="Condition Checker")
 
@@ -122,65 +137,97 @@ conditions.set("Select")
 
 
 def show1():
+    """
+    This function displays the selected shape in a label widget. The label widget is placed in the area_frame. The selected shape is obtained from the shapes variable using the get() method.
+    """
     mylabel = Label(area_frame, text=shapes.get()).pack()
 
 
 def show2():
+    """
+    This function displays the selected solid shape in a label widget. The label widget is placed in the volume_frame. The selected solid shape is obtained from the solidshape variable using the get() method.
+    """
     mylabel2 = Label(volume_frame, text=solidshape.get()).pack()
 
 
 def show3():
+    """
+    This function displays the selected condition in a label widget. The label widget is placed in the condition_frame. The selected condition is obtained from the conditions variable using the get() method.
+    """
     mylabel3 = Label(condition_frame, text=conditions.get()).pack()
 
+    # area frame option
 
-# area frame option
+
 shape_options = OptionMenu(
-    area_frame, shapes, "Circle", "Square", "Triangle", "Rectangle").pack()
-mybutton1 = Button(area_frame, text="Select shape", command=show1,bg="light blue").pack()
+    area_frame, shapes, "Circle", "Square", "Triangle", "Rectangle"
+).pack()
+mybutton1 = Button(
+    area_frame, text="Select shape", command=show1, bg="light blue"
+).pack()
 
-radius_side = Label(area_frame, text="Enter radius or side in m",bg="light blue").pack()
+radius_side = Label(
+    area_frame, text="Enter radius or side in m", bg="light blue"
+).pack()
 
-radius_entry = Entry(area_frame, font=("Helvetica", 20),bg="orange")
+radius_entry = Entry(area_frame, font=("Helvetica", 20), bg="orange")
 radius_entry.pack()
 
 height = Label(
-    area_frame, text="Enter height or width in m if applicable else enter 0",bg="light blue").pack()
-height_entry = Entry(area_frame, font=("Helvetica", 20),bg="orange")
+    area_frame,
+    text="Enter height or width in m if applicable else enter 0",
+    bg="light blue",
+).pack()
+height_entry = Entry(area_frame, font=("Helvetica", 20), bg="orange")
 height_entry.pack()
 
 
 # volume frame option
 volume_options = OptionMenu(
-    volume_frame, solidshape, "Cone", "Sphere", "Cylinder", "Cube", "Cuboid").pack()
-mybutton2 = Button(volume_frame, text="Select shape", command=show2,bg="light blue").pack()
-radius_side2 = Label(volume_frame, text="Enter radius or side in m",bg="light blue").pack()
-radius_entry2 = Entry(volume_frame, font=("Helvetica", 20),bg="orange")
+    volume_frame, solidshape, "Cone", "Sphere", "Cylinder", "Cube", "Cuboid"
+).pack()
+mybutton2 = Button(
+    volume_frame, text="Select shape", command=show2, bg="light blue"
+).pack()
+radius_side2 = Label(
+    volume_frame, text="Enter radius or side in m", bg="light blue"
+).pack()
+radius_entry2 = Entry(volume_frame, font=("Helvetica", 20), bg="orange")
 radius_entry2.pack()
 height2 = Label(
-    volume_frame, text="Enter height in m if applicable else enter 0",bg="light blue").pack()
-height2 = Entry(volume_frame, font=("Helvetica", 20),bg="orange")
+    volume_frame, text="Enter height in m if applicable else enter 0", bg="light blue"
+).pack()
+height2 = Entry(volume_frame, font=("Helvetica", 20), bg="orange")
 height2.pack()
 width = Label(
-    volume_frame, text="Enter breadth in m if applicable else enter 0",bg="light blue").pack()
-width_entry = Entry(volume_frame, font=("Helvetica", 20),bg="orange")
+    volume_frame, text="Enter breadth in m if applicable else enter 0", bg="light blue"
+).pack()
+width_entry = Entry(volume_frame, font=("Helvetica", 20), bg="orange")
 width_entry.pack()
 
 # condition frame
-condition_options = OptionMenu(condition_frame, conditions,
-                               "Pythagorean Triplet Checker", "Complimentary&Supplementary Angles").pack()
-mybutton3 = Button(condition_frame, text="Select condition",
-                   command=show3,bg="light blue").pack()
-side1_label = Label(condition_frame, text="Enter first number",bg="light blue").pack()
-side1_entry = Entry(condition_frame, font=("Helvetica", 20),bg="orange")
+condition_options = OptionMenu(
+    condition_frame,
+    conditions,
+    "Pythagorean Triplet Checker",
+    "Complimentary&Supplementary Angles",
+).pack()
+mybutton3 = Button(
+    condition_frame, text="Select condition", command=show3, bg="light blue"
+).pack()
+side1_label = Label(condition_frame, text="Enter first number", bg="light blue").pack()
+side1_entry = Entry(condition_frame, font=("Helvetica", 20), bg="orange")
 side1_entry.pack()
-side2_label = Label(condition_frame, text="Enter second number",bg="light blue").pack()
-side2_entry = Entry(condition_frame, font=("Helvetica", 20),bg="orange")
+side2_label = Label(condition_frame, text="Enter second number", bg="light blue").pack()
+side2_entry = Entry(condition_frame, font=("Helvetica", 20), bg="orange")
 side2_entry.pack()
-side3_label = Label(condition_frame, text="Enter third number",bg="light blue").pack()
-side3_entry = Entry(condition_frame, font=("Helvetica", 20),bg="orange")
+side3_label = Label(condition_frame, text="Enter third number", bg="light blue").pack()
+side3_entry = Entry(condition_frame, font=("Helvetica", 20), bg="orange")
 side3_entry.pack()
-angle_label = Label(condition_frame, text="Enter angle in degrees",bg="light blue").pack()
-angle_entry = Entry(condition_frame, font=("Helvetica", 20),bg="orange")
+angle_label = Label(
+    condition_frame, text="Enter angle in degrees", bg="light blue"
+).pack()
+angle_entry = Entry(condition_frame, font=("Helvetica", 20), bg="orange")
 angle_entry.pack()
 
 # button frame
@@ -192,11 +239,11 @@ button_frame3 = Frame(condition_frame)
 button_frame3.pack()
 
 # creating buttons
-button1 = Button(button_frame1, text="Calculate", command=area,bg="red")
+button1 = Button(button_frame1, text="Calculate", command=area, bg="red")
 button1.grid(row=0, column=0, padx=10)
-button2 = Button(button_frame2, text="Calculate", command=volume,bg="red")
+button2 = Button(button_frame2, text="Calculate", command=volume, bg="red")
 button2.grid(row=0, column=0, padx=10)
-button3 = Button(button_frame3, text="Calculate", command=condition_check,bg="red")
+button3 = Button(button_frame3, text="Calculate", command=condition_check, bg="red")
 button3.grid(row=0, column=0, padx=10)
 
 root.mainloop()
--- test.py
+++ test.py
@@ -6,9 +6,12 @@ import os
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-_redis = Redis(host=os.getenv('REDIS_HOSTNAME'), port=6379, password=os.getenv("REDIS_PASSWORD")) 
+_redis = Redis(
+    host=os.getenv("REDIS_HOSTNAME"), port=6379, password=os.getenv("REDIS_PASSWORD")
+)
 _redis_pubsub = _redis.pubsub()
-_redis_pubsub.subscribe('my-first-channel')
+_redis_pubsub.subscribe("my-first-channel")
+
 
 @app.before_request
 def checkConnectionRedis():
@@ -20,40 +23,49 @@ def checkConnectionRedis():
     try:
         _redis.ping()
     except _redis.ConnectionError:
-        redis = Redis(host='redis', port=6379)
+        redis = Redis(host="redis", port=6379)
         _redis_pubsub = _redis.pubsub()
-        _redis_pubsub.subscribe('my-first-channel')
+        _redis_pubsub.subscribe("my-first-channel")
+
 
 def getcounter():
     """
     This Python function retrieves the value of a counter from Redis and returns it as an integer. If the counter does not exist in Redis, it returns 0. The function uses the Redis `get` method to retrieve the counter value and decodes it from bytes to a string before converting it to an integer.
     """
-    return _redis.get('web_counter') and int(_redis.get('web_counter').decode("utf-8")) or 0
+    return (
+        _redis.get("web_counter")
+        and int(_redis.get("web_counter").decode("utf-8"))
+        or 0
+    )
 
-@app.route('/counter', methods=["GET"])
+
+@app.route("/counter", methods=["GET"])
 def get():
     """
     This Flask route returns the current value of a counter as a JSON object. The route uses the `getcounter` function to retrieve the counter value from Redis and returns it as a JSON object with the key `web_counter`. The route only accepts GET requests.
     """
     return jsonify(web_counter=getcounter())
 
-@app.route('/counter', methods=["POST"])
+
+@app.route("/counter", methods=["POST"])
 def create():
     """
     This Flask route increments the value of a counter in Redis by 1 and returns an empty string. The route uses the Redis `incr` method to increment the counter value. The route only accepts POST requests.
     """
-    _redis.incr('web_counter')
+    _redis.incr("web_counter")
     return ""
 
-@app.route('/counter', methods=["DELETE"])
+
+@app.route("/counter", methods=["DELETE"])
 def remove():
     """
     This Flask route resets the value of a counter in Redis to 0 and returns an empty string. The route uses the Redis `set` method to set the counter value to 0. The route only accepts DELETE requests.
     """
-    _redis.set('web_counter', 0)
+    _redis.set("web_counter", 0)
     return ""
 
-@app.route('/message', methods=["GET"])
+
+@app.route("/message", methods=["GET"])
 def getMessage():
     """
     This Flask route retrieves a message from a Redis pub/sub channel and returns it as a JSON object. The route logs information about the received message using the Flask logger. The route uses the Redis `get_message` method to retrieve the message from the pub/sub channel and returns it as a JSON object with the key `message`. The route only accepts GET requests.
@@ -61,15 +73,17 @@ def getMessage():
     current_app.logger.info("recieve data: " + str(_redis_pubsub.get_message()))
     return jsonify(message=str(_redis_pubsub.get_message()))
 
-@app.route('/message', methods=["POST"])
+
+@app.route("/message", methods=["POST"])
 def createMessage():
     """
     This Flask route publishes a message to a Redis pub/sub channel and returns an empty string. The message is generated randomly using the `randint` function. The route logs information about the published message using the Flask logger. The route uses the Redis `publish` method to publish the message to the pub/sub channel. The route only accepts POST requests.
     """
-    data = 'data ' + str(randint(0,10))
+    data = "data " + str(randint(0, 10))
     current_app.logger.info("publish data: " + str(data))
-    _redis.publish('my-first-channel', data)
+    _redis.publish("my-first-channel", data)
     return ""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
